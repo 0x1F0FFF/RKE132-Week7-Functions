3793844e315d4d95237f660f0badbfe2f9f55368
Morten <morten@scripts.ee>
2024-03-13 19:22:00 +0200

    Week 7 code

---
+++ Week7Functions/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.WriteLine("Are you in or out?");
+string userChoice = Console.ReadLine();
+
+if (userChoice == "in")
+{
+    PrintHello();
+}
+else
+{
+    PrintGoodbye();
+}
+
+static void PrintHello()
+{
+    Console.WriteLine("Hello, world!");
+}
+
+static void PrintGoodbye()
+{
+    Console.WriteLine("See you later, alligator");
+}
